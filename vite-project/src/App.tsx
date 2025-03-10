1518b1ea5093e85e0541d0c58c739d53c8896efa
Katarzyna Krawczyk <katarzynaklimek26@gmail.com>
2025-03-10 02:18:01 +0100

    test

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -13,6 +13,9 @@ function App() {
 				<a href='https://react.dev' target='_blank'>
 					<img src={reactLogo} className='logo react' alt='React logo' />
 				</a>
+				<a href='https://react.dev' target='_blank'>
+					<img src={reactLogo} className='logo react' alt='React logo' />
+				</a>
 			</div>
 
 			<h1>React + Typescript</h1>
